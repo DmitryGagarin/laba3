dfa4a4d97389e035d530bfe655ada4e04f18640b
DmitryGagarin <gagriu@yandex.ru>
2026-04-21 08:24:35 +0000

    Initial commit: FastAPI app

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="My Laba3 API", version="1.0.0")
+
+@app.get("/")
+def home():
+    return {"message": "Привет, мир!", "author": "Ваше Имя"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/about")
+def about():
+    return {"project": "Лабораторная работа 3", "server": "VPS"}
